0802cb77b1e876cb91eaa0b8f19ed164d822c075
wingstone <1812467051@qq.com>
2021-03-23 23:46:43 +0800

    ssr

---
+++ Assets/Scene/BuiltIn/Scripts/SSR/SSR.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using PostProcessing;
+
+// https://developer.download.nvidia.cn/assets/gamedev/files/sdk/11/FXAA_WhitePaper.pdf
+// http://blog.simonrodriguez.fr/articles/30-07-2016_implementing_fxaa.html
+
+/*
+attention:
+fxaa should use in srgb space;
+fxaa shold use in LDR;
+fxaa shold use bilinear filter;
+*/
+
+[ExecuteAlways]
+[RequireComponent(typeof(Camera))]
+public class SSR : MonoBehaviour
+{
+    //==== type
+
+    class ShaderStrings
+    {
+        public static string ssr = "Hidden/AdvancedRTR/SSR";
+    }
+    class ShaderConstants
+    {
+        public static int _SourceRT = Shader.PropertyToID("_SourceRT");
+        public static int _TestRT = Shader.PropertyToID("_TestResult");
+    }
+
+    //====mono
+
+    private void OnEnable()
+    {
+        Initial();
+        UpdateRender();
+    }
+
+    private void OnPreRender()
+    {
+        UpdateRender();
+    }
+
+    private void OnDisable()
+    {
+        EndRender();
+    }
+
+    //=====method
+
+    // only run in defer
+    bool isSupport()
+    {
+        bool support = true;
+        support = support && SystemInfo.supportsMotionVectors;
+        support = support && _Camera.actualRenderingPath == RenderingPath.DeferredShading;
+        support = support && SystemInfo.supportsComputeShaders;
+        return support;
+    }
+
+    DepthTextureMode GetCameraFlags()
+    {
+        return DepthTextureMode.Depth | DepthTextureMode.MotionVectors;
+    }
+    CameraEvent CameraPoint
+    {
+        get
+        {
+            return CameraEvent.BeforeImageEffectsOpaque;
+        }
+    }
+
+    void Initial()
+    {
+    }
+
+    private void Setting()
+    {
+        if (_SSRMat == null)
+        {
+            Shader ssr = Shader.Find(ShaderStrings.ssr);
+            _SSRMat = new Material(ssr);
+            _SSRMat.hideFlags = HideFlags.HideAndDontSave;
+        }
+        int size = 1;
+        var screenSpaceProjectionMatrix = new Matrix4x4();
+        screenSpaceProjectionMatrix.SetRow(0, new Vector4(size * 0.5f, 0f, 0f, size * 0.5f));
+        screenSpaceProjectionMatrix.SetRow(1, new Vector4(0f, size * 0.5f, 0f, size * 0.5f));
+        screenSpaceProjectionMatrix.SetRow(2, new Vector4(0f, 0f, 1f, 0f));
+        screenSpaceProjectionMatrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
+
+        var projectionMatrix = GL.GetGPUProjectionMatrix(_Camera.projectionMatrix, false);
+        screenSpaceProjectionMatrix = projectionMatrix;
+
+        _SSRMat.SetMatrix("_ViewMatrix", _Camera.worldToCameraMatrix);
+        _SSRMat.SetMatrix("_InverseViewMatrix", _Camera.worldToCameraMatrix.inverse);
+        _SSRMat.SetMatrix("_InverseProjectionMatrix", projectionMatrix.inverse);
+        _SSRMat.SetMatrix("_ScreenSpaceProjectionMatrix", projectionMatrix);
+        _SSRMat.SetFloat("_RayMatchStep", _RayMatchStep);
+    }
+
+    private void Render()
+    {
+        _CommandBuffer = new CommandBuffer();
+        _CommandBuffer.name = "AdvancedRTR";
+
+        _CommandBuffer.GetTemporaryRT(ShaderConstants._SourceRT, _Camera.pixelWidth, _Camera.pixelHeight, 0, FilterMode.Bilinear, RenderTextureFormat.ARGBHalf);
+
+        //copy
+        RuntimeUtilities.BuiltinBlit(_CommandBuffer, BuiltinRenderTextureType.CameraTarget, ShaderConstants._SourceRT);
+
+        //FXAA
+        _CommandBuffer.BeginSample("SSR");
+
+        _CommandBuffer.GetTemporaryRT(ShaderConstants._TestRT, _Camera.pixelWidth, _Camera.pixelHeight, 0, FilterMode.Bilinear, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+        RuntimeUtilities.BlitFullscreenTriangle(_CommandBuffer, ShaderConstants._SourceRT, ShaderConstants._TestRT, _SSRMat, 0);
+
+        RuntimeUtilities.BuiltinBlit(_CommandBuffer, ShaderConstants._TestRT, BuiltinRenderTextureType.CameraTarget);
+
+        _CommandBuffer.EndSample("SSR");
+
+        _CommandBuffer.ReleaseTemporaryRT(ShaderConstants._SourceRT);
+        _CommandBuffer.ReleaseTemporaryRT(ShaderConstants._TestRT);
+        _Camera.AddCommandBuffer(CameraPoint, _CommandBuffer);
+    }
+
+    private void UpdateRender()
+    {
+        if (_CommandBuffer != null)
+            _Camera.RemoveCommandBuffer(CameraPoint, _CommandBuffer);
+
+        Setting();
+        Render();
+    }
+
+    private void EndRender()
+    {
+        if (_CommandBuffer != null)
+            _Camera.RemoveCommandBuffer(CameraPoint, _CommandBuffer);
+    }
+
+    //===data
+    Camera _Camera
+    {
+        get
+        {
+            if (_camera == null)
+            {
+                _camera = GetComponent<Camera>();
+            }
+            return _camera;
+        }
+    }
+    Camera _camera = null;
+    CommandBuffer _CommandBuffer = null;
+    Material _SSRMat = null;
+    [SerializeField]
+    float _RayMatchStep = 0.01f;
+}
---
+++ Assets/Scene/Shaders/SSR.shader
@@ -0,0 +1,105 @@
+﻿Shader "Hidden/AdvancedRTR/SSR"
+{
+    Properties
+    {
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            Name "Ray Matching Test"
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #define MLAA_THRELOD 0.1
+
+            #include "UnityCG.cginc"
+            #include "Common.cginc"
+
+            sampler2D _MainTex;
+            float4 _MainTex_TexelSize;
+
+            sampler2D _CameraGBufferTexture0; // albedo = g[0].rgb
+            sampler2D _CameraGBufferTexture1; // roughness = g[1].a
+            sampler2D _CameraGBufferTexture2; // normal.xyz 2. * g[2].rgb - 1.
+            sampler2D _CameraDepthTexture; //depth = r;
+
+            float4x4 _ViewMatrix;
+            float4x4 _InverseViewMatrix;
+            float4x4 _InverseProjectionMatrix;
+            float4x4 _ScreenSpaceProjectionMatrix;
+            float _RayMatchStep;
+
+            struct Result
+            {
+                float2 uv;
+                float4 color;
+            };
+
+            struct Ray
+            {
+                float3 origin;
+                float3 direction;
+            };
+
+            float3 GetViewSpacePosition(float2 uv)
+            {
+                float depth = tex2D(_CameraDepthTexture, uv).r;
+                float4 result = mul(_InverseProjectionMatrix, float4(2.0 * uv - 1.0, depth, 1.0));
+                return result.xyz / result.w;
+            }
+
+            Result RayMatching(Ray ray)
+            {
+                Result result = (Result)0;
+                
+                float matchStep = min(_MainTex_TexelSize.x, _MainTex_TexelSize.y);
+                for(int i = 0; i < 32; i++)
+                {
+                    ray.origin += ray.direction*_RayMatchStep * matchStep;
+                    float4 position = mul(_ScreenSpaceProjectionMatrix, float4(ray.origin, 1));
+                    position.xyz = position.xyz / position.w;
+                    float2 uv = position.xy*0.5+0.5;
+
+                    if(uv.x < 0 || uv.x > 1 || uv.y < 0 || uv.y > 1 )
+                    {
+                        break;
+                    }
+
+                    float realZ = position.z;
+                    if(tex2D(_CameraDepthTexture, uv).r > realZ)
+                    {
+                        result.uv = uv;
+                        result.color = tex2D(_MainTex, uv);
+                        // result.color.rg = uv;
+                        // result.color.b = 0;
+                        break;
+                    }
+                }
+                
+                return result;
+            }
+
+            float4 frag(VaryingsDefault i) : SV_Target0
+            {
+                float3 normal = tex2D(_CameraGBufferTexture2, i.uv).rgb*2.0-1.0;
+                normal = mul(_ViewMatrix, normal);
+
+                Ray ray = (Ray)0;
+                ray.origin = GetViewSpacePosition(i.uv);
+                ray.direction = reflect(normalize(ray.origin), normal);
+
+                Result result = RayMatching(ray);
+
+                // return float4(ray.direction, 1);
+
+                return result.color;
+            }
+            ENDCG
+        }
+    }
+}
