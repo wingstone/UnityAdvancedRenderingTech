3c16abef716a15031a38555eb8da17aba5d497d4
wingstone <1812467051@qq.com>
2021-08-20 09:20:37 +0800

    sspr

--- Assets/Scene/BuiltIn/Scripts/SSPR/SSPR.cs
+++ Assets/Scene/BuiltIn/Scripts/SSPR/SSPR.cs
@@ -29,6 +29,7 @@ public class SSPR : MonoBehaviour
         public static int _InverseProjectionMatrix = Shader.PropertyToID("_InverseProjectionMatrix");
         public static int _ProjectionMatrix = Shader.PropertyToID("_ProjectionMatrix");
         public static int _ViewMatrix = Shader.PropertyToID("_ViewMatrix");
+        public static int _CameraPos = Shader.PropertyToID("_CameraPos");
         public static int _PlaneHeight = Shader.PropertyToID("_PlaneHeight");
         public static int _PlaneBoxMin = Shader.PropertyToID("_PlaneBoxMin");
         public static int _PlaneBoxMax = Shader.PropertyToID("_PlaneBoxMax");
@@ -85,6 +86,9 @@ public class SSPR : MonoBehaviour
         _CommandBuffer.BeginSample("SSPR");
         _CommandBuffer.GetTemporaryRT(ShaderConstants._ResultRT, _Camera.pixelWidth, _Camera.pixelHeight, 0, FilterMode.Bilinear, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Linear, 1, true);
 
+        _CommandBuffer.SetRenderTarget(ShaderConstants._ResultRT);
+        _CommandBuffer.ClearRenderTarget(false, true, new Color(0, 0, 0, 0));
+
         int kernel = computeShader.FindKernel("CSMain");
         int width = _Camera.pixelWidth;
         int height = _Camera.pixelHeight;
@@ -92,15 +96,17 @@ public class SSPR : MonoBehaviour
         _CommandBuffer.SetComputeTextureParam(computeShader, kernel, ShaderConstants._ResultRT, ShaderConstants._ResultRT);
         _CommandBuffer.SetComputeTextureParam(computeShader, kernel, ShaderConstants._CameraDepthTexture, BuiltinRenderTextureType.ResolvedDepth);
         _CommandBuffer.SetComputeVectorParam(computeShader, ShaderConstants._TextureSize, new Vector4(1f / width, 1f / height, width, height));
+        _CommandBuffer.SetComputeVectorParam(computeShader, ShaderConstants._CameraPos, _Camera.transform.position);
         _CommandBuffer.SetComputeFloatParam(computeShader, ShaderConstants._PlaneHeight, _height);
         _CommandBuffer.SetComputeVectorParam(computeShader, ShaderConstants._PlaneBoxMin, _boxmin);
         _CommandBuffer.SetComputeVectorParam(computeShader, ShaderConstants._PlaneBoxMax, _boxmax);
-        
+
         var projectionMatrix = GL.GetGPUProjectionMatrix(_Camera.projectionMatrix, false);
-        var vp = projectionMatrix*_Camera.worldToCameraMatrix;
+        var vp = projectionMatrix * _Camera.worldToCameraMatrix;
 
         _CommandBuffer.SetComputeMatrixParam(computeShader, ShaderConstants._InverseProjectionMatrix, vp.inverse);
         _CommandBuffer.SetComputeMatrixParam(computeShader, ShaderConstants._ProjectionMatrix, vp);
+        _CommandBuffer.SetComputeMatrixParam(computeShader, ShaderConstants._ViewMatrix, _Camera.worldToCameraMatrix);
 
         _CommandBuffer.DispatchCompute(computeShader, kernel, Mathf.CeilToInt(width / 8f), Mathf.CeilToInt(height / 8f), 1);
 
@@ -126,6 +132,20 @@ public class SSPR : MonoBehaviour
     {
         if (_CommandBuffer != null)
             _Camera.RemoveCommandBuffer(CameraEvent.BeforeImageEffects, _CommandBuffer);
+    }
+
+    // gizmos
+    private void OnDrawGizmos()
+    {
+        Vector3 minPos = new Vector3(_boxmin.x, _height, _boxmin.y);
+        Vector3 maxPos = new Vector3(_boxmax.x, _height, _boxmax.y);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(minPos, minPos + new Vector3(0, 0, maxPos.z - minPos.z));
+        Gizmos.DrawLine(minPos, minPos + new Vector3(maxPos.x - minPos.x, 0, 0));
+        Gizmos.DrawLine(maxPos - new Vector3(0, 0, maxPos.z - minPos.z), maxPos);
+        Gizmos.DrawLine(maxPos - new Vector3(maxPos.x - minPos.x, 0, 0), maxPos);
+        Gizmos.color = Color.white;
     }
 
     //===data
